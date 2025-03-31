6d1cc3990f1e756714fa1fe9da813ea60daf24f7
gustavogordoni <gustavogordoni1@gmail.com>
2025-03-30 21:49:53 -0300

    03: lists with ListView and ListTitle

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -3,7 +3,6 @@ import 'package:agenda_crud/app/view/contact_list.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
-
   // ROTAS
   static const HOME = '/';
   static const CONTACT_FORM = 'contact-form';
@@ -12,8 +11,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      // theme: ThemeData(
+      //   primarySwatch: Colors.blue,
+      // ),
+      theme: ThemeData(        
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.blue,    
+        ),
       ),
       routes: {
         HOME: (context) => ContactList(),
--- lib/app/view/contact_list.dart
+++ lib/app/view/contact_list.dart
@@ -2,6 +2,39 @@ import 'package:agenda_crud/app/my_app.dart';
 import 'package:flutter/material.dart';
 
 class ContactList extends StatelessWidget {
+  final listaContatos = [
+    {
+      'nome': 'Pietro',
+      'telefone': '(19) 9 9874-6565',
+      'avatar': 'https://www.w3schools.com/howto/img_avatar.png',
+    },
+    {
+      'nome': 'Maria',
+      'telefone': '(11) 9 9813-1565',
+      'avatar': 'https://www.w3schools.com/howto/img_avatar.png',
+    },
+    {
+      'nome': 'Joao',
+      'telefone': '(17) 9 9889-5654',
+      'avatar': 'https://www.w3schools.com/howto/img_avatar.png',
+    },
+    {
+      'nome': 'Pedro',
+      'telefone': '(11) 9 9883-5256',
+      'avatar': 'https://www.w3schools.com/howto/img_avatar.png',
+    },
+    {
+      'nome': 'Paulo',
+      'telefone': '(11) 9 9579-1566',
+      'avatar': 'https://www.w3schools.com/howto/img_avatar.png',
+    },
+    {
+      'nome': 'Joana',
+      'telefone': '(17) 9 9843-5731',
+      'avatar': 'https://www.w3schools.com/howto/img_avatar.png',
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +49,31 @@ class ContactList extends StatelessWidget {
           ),
         ],
       ),
-      body: ListView(),
+      body: ListView.builder(
+        itemCount: listaContatos.length,
+        itemBuilder: (context, index) {
+          var contato = listaContatos[index];
+          var avatar = CircleAvatar(
+            backgroundImage: NetworkImage(contato['avatar']!),
+          );
+
+          return ListTile(
+            leading: avatar,
+            title: Text(contato['nome']!),
+            subtitle: Text(contato['telefone']!),
+
+            trailing: Container(
+              width: 100,
+              child: Row(
+                children: [
+                  IconButton(icon: Icon(Icons.edit), onPressed: null),
+                  IconButton(icon: Icon(Icons.delete), onPressed: null),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
