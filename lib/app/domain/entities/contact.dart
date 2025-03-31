9fdb7ba57d2eab19a8cded93b7b40abe34e28487
gustavogordoni <gustavogordoni1@gmail.com>
2025-03-31 09:44:05 -0300

    06: entity class

---
+++ lib/app/domain/entities/contact.dart
@@ -0,0 +1,17 @@
+class Contact {
+  int id;
+  String nome;
+  String telefone;
+  String email;
+  String urlAvatar;
+
+  // ENCAPSULAMENTO 
+  // int get id => _id;
+  // String get nome => _nome;
+  // String get telefone => _telefone;
+  // String get email => _email;
+  // String get urlAvatar => _urlAvatar;
+
+  Contact({required this.id, required this.nome, required this.telefone, required this.email, required this.urlAvatar});
+  
+}
