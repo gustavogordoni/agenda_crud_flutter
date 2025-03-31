51b95803b42ea37844e2ec9243b15d55b0ea4376
gustavogordoni <gustavogordoni1@gmail.com>
2025-03-31 08:29:26 -0300

    04: script database and add dependecie SQFLite

---
+++ lib/app/database/script.dart
@@ -0,0 +1,20 @@
+final createTable = '''
+  CREATE TABLE contact (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome VARCHR(200) NOT NULL,
+    telefone CHAR(16) NOT NULL,
+    email VARCHR(150) NOT NULLNOT NULL,
+    url_avatar VARCHR(300) NOT NULL
+  );
+
+''';
+
+final insertValues = '''
+  INSERT INTO contact (nome, telefone, email, url_avatar) VALUES 
+    ('Pietro', '(19) 9 9874-6565', 'pietro@email.com', 'https://www.w3schools.com/howto/img_avatar.png'),
+    ('Maria', '(11) 9 9813-1565', 'maria@email.com', 'https://www.w3schools.com/howto/img_avatar.png'),
+    ('João', '(17) 9 9889-5654', 'joao@email.com', 'https://www.w3schools.com/howto/img_avatar.png'),
+    ('Pedro', '(11) 9 9883-5256', '@pedroemail.com', 'https://www.w3schools.com/howto/img_avatar.png'),
+    ('Paulo', '(11) 9 9579-1566', 'paulo@email.com', 'https://www.w3schools.com/howto/img_avatar.png'),
+    ('Joana', '(17) 9 9843-5731', 'joana@email.com', 'https://www.w3schools.com/howto/img_avatar.png');
+''';
