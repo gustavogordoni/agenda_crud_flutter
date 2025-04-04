8272f01d6ac0f9a358ee0679b6e47084444a5cde
gustavogordoni <gustavogordoni1@gmail.com>
2025-04-04 11:48:49 -0300

    10: domain rules

--- lib/app/domain/services/contact_service.dart
+++ lib/app/domain/services/contact_service.dart
@@ -1,7 +1,56 @@
+import 'package:agenda_crud/app/domain/entities/contact.dart';
+import 'package:agenda_crud/app/domain/exception/domain_layer_exception.dart';
 import 'package:agenda_crud/app/domain/interfaces/contact_dao.dart';
 import 'package:get_it/get_it.dart';
 
 class ContactService {
   
-  var dao = GetIt.I.get<ContactDAO>();
+  var _dao = GetIt.I.get<ContactDAO>();
+
+  save(Contact contact) {
+    validateNome(contact.nome);
+    validateEmail(contact.email);
+    validatePhone(contact.telefone);
+    _dao.save(contact);
+  }
+
+  remove(int id){
+    _dao.remove(id);
+  }
+
+  Future<List<Contact>> find() {
+    return _dao.find();
+  }
+
+  // VALIDAÇÕES NA CAMADA DE DOMÍNIO
+  validateNome(String name){
+    var min = 3;
+    var max = 50;
+
+    if(name == null){
+      throw new DomainLayerException('O nome é obrigatório.');
+    }else if(name.length < min){
+      throw new DomainLayerException('O nome deve ter pelo menos $min caracteres.');
+    }else if(name.length > max){
+      throw new DomainLayerException('O nome deve ter no máximo $max caracteres.');
+    }
+  }
+
+  validateEmail(String email){
+    if(email == null){
+      throw new DomainLayerException('O e-mail é obrigatório.');
+    }else if(!email.contains('@')){
+      throw new DomainLayerException('O e-mail deve conter um @.');
+    }
+  }
+
+  validatePhone(String phone){
+    var format = RegExp(r'^\([0-9]{2}\) [9] [6-9]{1}[0-9]{3}\-[0-9]{4}$');
+
+    if(phone == null){
+      throw new DomainLayerException('O telefone é obrigatório.');
+    }else if(!format.hasMatch(phone)){
+      throw new DomainLayerException('O telefone deve estar no formato (99) 9 9999-9999.');
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:agenda_crud/app/my_app.dart';
 
+
 void main() {
   sqfliteFfiInit();
   databaseFactory = databaseFactoryFfi;
