de3abe0440ef73d5d2752faa03acb47b280999c5
gustavogordoni <gustavogordoni1@gmail.com>
2025-04-07 17:11:35 -0300

    19: config firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,32 @@
 import 'dart:io';
 
+import 'package:agenda_crud/firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:agenda_crud/app/injection.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:agenda_crud/app/my_app.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
-  
   if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
     sqfliteFfiInit();
     databaseFactory = databaseFactoryFfi;
   }
 
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   setupInjection();
   runApp(MyApp());
+  
+  FirebaseFirestore.instance.collection('contact').doc().set({
+    'name': 'Gustavo',
+    'phone': '(11) 9 999-999',
+    'email': 'H4a0f@example.com',
+  });
 }
+
